f761d5d498811bd032548b58a142a21b89818864
Aura Nabila <auranabila.baf@gmail.com>
2025-12-15 14:46:09 +0700

    Add files via upload

---
+++ latihan5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LATIHAN 5 PEMROGRAMAN WEB</title>
+    </head>
+    <body>
+        <?php
+            $nim = "2305101089";
+            $nama = "Aura Nabila";
+            $angka1 = 10;
+            $angka2 = 15;
+
+            $hasilpenjumlahan = $angka1 + $angka2;
+
+            echo "NIM : ". $nim. "<br/>";
+            echo "Nama : ". $nama. "<br/>";
+            echo "Hasil Penjumlahan dari " . $angka1. " dan " . $angka2. " = " . $hasilpenjumlahan."<br/>";
+        ?>
+    </body>
+</html>
